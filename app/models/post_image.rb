00a0fd19ec880a96c59444bc42c8146dcf16b619
tootaemi <n4mtkk@gmail.com>
2023-09-27 13:01:00 +0000

    [Update]インデント修正

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,36 +1,31 @@
 class PostImage < ApplicationRecord
-
   has_one_attached :image
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-
 
   validates :shop_name, presence: true
   validates :image, presence: true
   #shop_nameが存在しているかを確認するバリデーション
   #imageが存在しているかを確認するバリデーション
 
-
-   def get_image
+  def get_image
     if image.attached?
       image
     else
-      'no_image.jpg'
+      "no_image.jpg"
     end
-   end
+  end
 
-    def get_image
+  def get_image
     unless image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      file_path = Rails.root.join("app/assets/images/no_image.jpg")
+      image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
     end
     image
-    end
-
+  end
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-
 end
